26d169f9981752083cba958b4de7829018cb6b2e
Kevin Cunningham <github@kevincunningham.co.uk>
2022-12-06 06:29:28 +0000

    day 6

---
+++ 06.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require("node:fs");
+
+const data = readFileSync("./06-input").toString();
+
+console.log(indexAtStartOfPacket(data));
+console.log(indexAtStartOfMessage(data));
+
+function indexAtStartOfPacket(data) {
+  let currentList = [];
+  for (let i = 0; i < data.length; i++) {
+    const char = data[i];
+    if (currentList.includes(char)) {
+      // Need to handle the characters between this and the other occurance
+      const indexOfFirst = currentList.indexOf(char);
+      currentList = currentList.splice(indexOfFirst + 1);
+      currentList.push(char);
+    } else {
+      currentList.push(char);
+    }
+    if (currentList.length === 4) {
+      return i + 1;
+    }
+  }
+}
+
+function indexAtStartOfMessage(data) {
+  let currentList = [];
+  for (let i = 0; i < data.length; i++) {
+    const char = data[i];
+    if (currentList.includes(char)) {
+      // Need to handle the characters between this and the other occurance
+      const indexOfFirst = currentList.indexOf(char);
+      currentList = currentList.splice(indexOfFirst + 1);
+      currentList.push(char);
+    } else {
+      currentList.push(char);
+    }
+    if (currentList.length === 14) {
+      return i + 1;
+    }
+  }
+}
