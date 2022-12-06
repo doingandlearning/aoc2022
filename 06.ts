460c35cc2dc04caaae68c44d90d8321108cab84b
Kevin Cunningham <github@kevincunningham.co.uk>
2022-12-06 06:47:39 +0000

    Remove comment.

--- 06.ts
+++ 06.ts
@@ -10,7 +10,6 @@ function indexAtEndOfRepetition(data: string, target: number) {
   for (let i = 0; i < data.length; i++) {
     const char = data[i];
     if (currentList.includes(char)) {
-      // Need to handle the characters between this and the other occurance
       const indexOfFirst = currentList.indexOf(char);
       currentList = currentList.splice(indexOfFirst + 1);
       currentList.push(char);
